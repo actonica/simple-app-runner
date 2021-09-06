3b8d09302a676f7952a82a267047fd7cb25732f3
actonica-publisher <company@actonica.ru>
2021-09-06 22:44:38 +0700

    huge refactor
    - add initContainer
    - add env vars
    - add claims (PV)
    - fix annotations for ingress to use from command line with --set
    - bump ver to 1.14

--- simple-app-runner/template.bat
+++ simple-app-runner/template.bat
@@ -1,1 +1,25 @@
-helm template . -f values.yaml -n app-chart-namespace --set app.service.extra[0].name=images --set app.service.extra[0].spec.type=ExternalName --set app.service.extra[0].spec.externalName=server.com --set app.ingressExtra[0].name=images --set app.ingressExtra[0].path=/images --set app.ingressExtra[0].servicePort=443 --set app.ingressExtra[0].backendHttps=true 1> template.yaml
+rem     --set app.ingressExtra[0].backendHttps=true
+rem     --set app.volumes[0].name=nfs-1 ^
+rem     --set app.volumes[0].spec.persistentVolumeClaim.claimName=nfs-provisioner-1 ^
+rem     --set app.volumeMounts[0].name=nfs-1 ^
+rem     --set app.volumeMounts[0].mountPath=/export ^
+rem     --set app.service.extra[0].name=images ^
+rem     --set app.service.extra[0].spec.type=ExternalName ^
+rem     --set app.service.extra[0].spec.externalName=server.com ^
+rem     --set app.ingressExtra[0].name=images ^
+rem    --set app.ingressExtra[0].path=/images ^
+rem    --set app.ingressExtra[0].servicePort=443 ^
+
+helm template . -f values.yaml -n app-chart-namespace ^
+  --set app.env.VXXXAR1=valussse1 ^
+  --set app.env.VAVVVR2=valssddue2 ^
+  --set app.ingress.annotations.nginx\.ingress\.kubernetes\.io\/permanent-redirect-code=301 ^
+  --set app.initContainer.image.repository=busybox ^
+  --set app.initContainer.image.tag=latest ^
+  --set app.volumes[0].name=nfs-1 ^
+  --set app.volumes[0].claimName=claim1 ^
+  --set app.volumes[0].mountPath=/export ^
+  --set app.configs[0].name=appsettings.json ^
+  --set app.configs[0].path=/app ^
+  --set app.configs[0].content=a123 1> template.yaml
+
